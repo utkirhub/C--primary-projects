5e1bd156ca316d8fb85b1405770787e29f027a23
admin__217 <92862266+utkirhub@users.noreply.github.com>
2023-04-13 00:05:24 +0500

    C# Son Topish O'yini

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace World;
+class Mono
+{
+    static void Main(string[] args)
+    {
+        int x = 9;
+        int y = 0;
+        Console.WriteLine("Salom Son Topish O'yiniga Xush Kelibsiz!\n");
+        Console.WriteLine("Men Bir Son O'yladim Siz Uni Topishga Xarakat Qilib Ko'ring!");
+        while (y != x)
+        {
+            Console.Write("\nSon Kiriting: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            if (z > x)
+            {
+                Console.WriteLine("\nYo'q Men O'ylagan Son" + " " + z + " Sonidan Kichkina 👇");
+                Console.WriteLine("Qayta Urinib Ko'ring! ✌");
+            }
+            else if (z < x)
+            {
+                Console.WriteLine("\nYo'q Men O'ylagan Son" + " " + z + " Sonidan Katta 👆");
+                Console.WriteLine("Qayta Urinib Ko'ring! ✌");
+            }
+            else
+            {
+                Console.WriteLine("\nTopdingiz Tabriklayman 👏 Men" + " " + x + " " + "Sonni O'ylagan Edim 🙂");
+                break;
+            }
+        }
+    }
+}
+
